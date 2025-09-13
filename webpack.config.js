631a577140a632f9e0ba45879446eb457e4d504f
Subhadip Banerjee <subhadip@Subhadips-MacBook-Pro.local>
2025-09-13 12:11:08 +0530

    Production-ready webpack config + Vercel routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,63 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-    publicPath: '/',
-  },
-  resolve: { extensions: ['.js', '.jsx'] },
-  module: {
-    rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, use: 'babel-loader' },
-      { test: /\.css$/i, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-      { test: /\.(png|jpe?g|gif|svg)$/i, type: 'asset/resource' }
-    ]
-  },
-  plugins: [ new HtmlWebpackPlugin({ template: './public/index.html' }) ],
-  devServer: { static: path.join(__dirname, 'public'), historyApiFallback: true, port: 3000, hot: true, open: true },
-  mode: 'development'
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? 'bundle.[contenthash].js' : 'bundle.js',
+      clean: true,
+      publicPath: '/', // 🔑 Important for SPA routing
+    },
+    resolve: { extensions: ['.js', '.jsx'] },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      ...(isProd ? [new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })] : []),
+    ],
+    optimization: isProd
+      ? {
+          minimize: true,
+          minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+          splitChunks: { chunks: 'all' },
+        }
+      : {},
+    devServer: {
+      static: path.join(__dirname, 'public'),
+      historyApiFallback: true, // 🔑 SPA routing
+      port: 3000,
+      hot: true,
+      open: true,
+    },
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? 'source-map' : 'eval-source-map',
+  };
 };
